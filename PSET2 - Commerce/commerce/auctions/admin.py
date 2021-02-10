225cf4202cd2426372506d39a8671075c5fc528f
Mariano <mcoutada@gmail.com>
2021-02-10 20:54:06 -0300

    ...

--- PSET2 - Commerce/commerce/auctions/admin.py
+++ PSET2 - Commerce/commerce/auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Category, Listing, Comment, Watchlist
+
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Listing)
+admin.site.register(Comment)
+admin.site.register(Watchlist)
