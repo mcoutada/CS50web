7608c77f502a848a6244733a26e767dd4d3a7a09
Mariano <mcoutada@gmail.com>
2021-03-02 21:46:46 -0300

    end chapter 2

--- aditional/7-buildproshop-udemy-react+django/ecommerce/frontend/src/App.js
+++ aditional/7-buildproshop-udemy-react+django/ecommerce/frontend/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      My App 
-    </div>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
