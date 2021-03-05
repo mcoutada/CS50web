0e085ee0585ab1c8982820477e89091e667fe4c9
Mariano <mcoutada@gmail.com>
2021-03-04 23:30:10 -0300

    end chapter 5

--- aditional/7-buildproshop-udemy-react+django/ecommerce/frontend/src/App.js
+++ aditional/7-buildproshop-udemy-react+django/ecommerce/frontend/src/App.js
@@ -2,6 +2,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
+import CartScreen from "./screens/CartScreen";
 import ProductScreen from "./screens/ProductScreen";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
@@ -13,6 +14,7 @@ function App() {
         <Container>
           <Route path="/" component={HomeScreen} exact />
           <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
