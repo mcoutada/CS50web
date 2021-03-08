c47242d8b283d35b6205089a889624c9123a7659
Mariano <mcoutada@gmail.com>
2021-03-08 13:47:22 -0300

    ...

---
+++ PSET4_Network/project4/frontend/src/components/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+// import {render} from react-DOMError;
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ PSET4_Network/project4/frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App'
+
+const AppDiv = document.getElementById('app');
+ReactDOM.render(<div><App/></div>, AppDiv);
